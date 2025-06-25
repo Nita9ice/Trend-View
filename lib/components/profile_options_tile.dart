65d5464b4dd1e8500283682f7fbbb5225f30a4c7
Nita9ice <erhonsele1@gmail.com>
2025-06-25 19:04:36 +0100

    final cleanup and polish code

--- lib/components/profile_options_tile.dart
+++ lib/components/profile_options_tile.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+// Reusable class for profile menu items with an icon, label, and tap action.
 class ProfileOptionTile extends StatelessWidget {
+  // properties for this class
   final IconData icon;
   final String label;
   final VoidCallback onTap;
 
+  // initializing the properties
   const ProfileOptionTile({
     super.key,
     required this.icon,
@@ -14,19 +17,21 @@ class ProfileOptionTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get the current app theme (light or dark)
     final theme = Theme.of(context);
 
     return ListTile(
       leading: Icon(
         icon,
-        color: theme.iconTheme.color, // Use theme icon color
+        //  uses red color if it's a favorite icon, otherwise uses the theme's default icon color
+        color:
+            icon == Icons.favorite
+                ? Color.fromRGBO(231, 63, 63, 1)
+                : theme.iconTheme.color,
       ),
       title: Text(
         label,
-        style: TextStyle(
-          color: theme.textTheme.bodyLarge?.color, // Use theme text color
-          fontSize: 16,
-        ),
+        style: TextStyle(color: theme.textTheme.bodyLarge?.color, fontSize: 16),
       ),
       onTap: onTap,
     );
